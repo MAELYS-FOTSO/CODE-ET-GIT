de4694e567ed6555d658a9b53679a3ecf313ca36
MAELYS-FOTSO <matiedjm@3il.fr>
2024-11-26 16:20:30 +0100

    Modification de l'exercice 2 en refractionnant les methodes et en ajoutant les cartouches

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,44 @@
 public class GestionnaireNotes {
+	/*
+	 * description de la fonction afficherNotesMoyennes
+	 * affiche la note et la moyenne
+	 * @param nomEtudiant:nom de l'etudiant
+	 * 
+	 * @param notes:tableau de notes de l'etudiant
+	 */
+	public void afficheNotesMoyenne(String nomEtudiant, int[] notes) {
+		afficherNotes(nomEtudiant,notes);
+		System.out.println("Notes de " + nomEtudiant + ":");
+		double moyenne =0;
+		afficherMoyenne(nomEtudiant,notes);
+		System.out.println("Moyenne : " + moyenne);
+	}
+	/*
+	 * description de la fonction afficherNotes
+	 * affiche les notes
+	 * @param nomEtudiant:nom de l'etudiant
+	 * 
+	 * @param notes:tableau de notes de l'etudiant
+	 */
+	public void afficherNotes(String nomEtudiant, int[] notes) {
+			for (int note : notes) {
+			System.out.println("- " + note);
+		}
+	}
 
-    // Affiche note et moyennes
-    public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
-        System.out.println("Notes de " + nomEtudiant + ":");
-        for (int note : notes) {
-            System.out.println("- " + note);
-        }
-
-        // Calcul de la somme des notes
-        int somme = 0;
-        for (int note : notes) {
-            somme += note;
-        }
-
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
-    }
+	/*
+	 * description de la fonction afficherMoyennes
+	 * affiche la moyenne
+	 * @param nomEtudiant:nom de l'etudiant
+	 * 
+	 * @param notes:tableau de notes de l'etudiant
+	 */
+	public void afficherMoyenne(String nomEtudiant, int[] notes) {
+		int somme = 0;
+		for (int note : notes) {
+			somme += note;
+		}
+		double moyenne = (double) somme / notes.length;
+		System.out.println("Moyenne : " + moyenne);
+	}
 }
